6bac7d2ae5b3f1797511d05472de3729576ce8f7
Talal Ahmed <56167664+talal02@users.noreply.github.com>
2022-01-30 18:50:34 +0500

    Update vprotect.py

--- vprotect.py
+++ vprotect.py
@@ -6,8 +6,10 @@ os.system('pip3 install pypasser')
 
 import time as sleepy
 import sys
+from selenium.webdriver.chrome.service import Service
 from datetime import datetime
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 from fake_useragent import UserAgent
@@ -40,30 +42,47 @@ while 1:
     profile.set_preference("dom.webdriver.enabled", False)
     profile.set_preference("useAutomationExtension", False)
     profile.set_preference("marionette.enabled", False)
-    profile.set_preference('network.proxy.type', 1)
-    profile.set_preference('network.proxy.socks', '127.0.0.1')
-    profile.set_preference('network.proxy.socks_port', 9050)
     profile.update_preferences()
     browser = webdriver.Firefox(firefox_profile=profile)
+    # s = Service('./chromedriver.exe')
+    # browser = webdriver.Chrome(service=s)
+    browser.maximize_window()
     browser.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
-    #print(profile)
-    #browser.get("https://licenses.storware.eu/vprotect/trial/")
-    browser.get("https://licenses.storware.eu/registration/vprotect?type=TRIAL")
-    #sleepy.sleep(20)
-    #perhost = browser.find_element_by_xpath("/html/body/app-root/app-layout/app-license-wizard/form/div[1]/div/div/app-license-wizard-form-models/div/div/div[1]/label")
-    #perhost.click()
-    #hyper = browser.find_element_by_xpath("/html/body/app-root/app-layout/app-license-wizard/form/div[1]/div/div/accordion/accordion-group[1]/div/div[1]/div/div/div")
-    #hyper.click()
-    #esxi = browser.find_element_by_xpath("/html/body/app-root/app-layout/app-license-wizard/form/div[1]/div/div/accordion/accordion-group[1]/div/div[2]/div/app-license-wizard-form-virtual-platforms/div/div/div[5]/div[1]/div/label")
-    #esxi.click()
-    #esxi_host = browser.find_element_by_xpath("/html/body/app-root/app-layout/app-license-wizard/form/div[1]/div/div/accordion/accordion-group[1]/div/div[2]/div/app-license-wizard-form-virtual-platforms/div/div/div[5]/div[2]/input")
-    #esxi_host.clear()
-    #sleepy.sleep(2)
-    #esxi_host.send_keys("2")
-    #next_step = browser.find_element_by_xpath('//*[@id="proceedButton"]')
-    #next_step = browser.find_element_by_id("proceedButton")
-    #next_step.click()
-    #sleepy.sleep(5)
+    browser.get("https://storware.eu/licenses/vprotect/trial/")
+    sleepy.sleep(10)
+    browser.switch_to.frame(browser.find_element(By.ID, 'calculatorFrame'))
+    browser.execute_script("document.getElementsByClassName('custom-control custom-radio custom-control-inline')[0].children[0].click()")
+    browser.execute_script("document.getElementsByClassName('panel card panel-default')[0].children[0].click()")
+    browser.execute_script("document.getElementsByClassName('custom-control custom-checkbox')[4].children[0].click()")
+    value = browser.execute_script("return document.getElementsByClassName('form-control ng-untouched ng-pristine ng-valid')[0]")
+    try:
+        value.send_keys(Keys.CONTROL + 'a')
+        value.send_keys('2')
+    except:
+        value.click()
+        value.send_keys(Keys.CONTROL + 'a')
+        value.send_keys('2')
+
+    browser.switch_to.default_content()
+    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    sleepy.sleep(10)
+    browser.switch_to.frame(browser.find_element(By.ID, 'calculatorFrame'))
+    sleepy.sleep(1)
+    try:
+        zbtn = browser.find_element(By.ID, 'proceedButton')
+        print(f'1. {zbtn.text}')
+        zbtn.click()
+        try:
+            print(browser.execute_script("return document.getElementById('proceedButton').textContent"))
+            browser.execute_script("document.getElementById('proceedButton').click()")
+        except:
+            pass
+    except:
+        pass
+    sleepy.sleep(4)
+
+
+
     fname = browser.find_element_by_xpath('//*[@id="first-name"]')
     lname = browser.find_element_by_xpath('//*[@id="last-name"]')
     fname.send_keys("David")
@@ -84,16 +103,29 @@ while 1:
     role.click()
     zipcode = browser.find_element_by_xpath('//*[@id="zipCode"]')
     zipcode.send_keys("91119")
-    country = browser.find_element_by_xpath("/html/body/app-root/app-layout/app-registration-form/form/div/div[1]/div/div[7]/div[3]/ngx-select-dropdown/div/div/ul[2]/li")
+    sleepy.sleep(2)
+    country = browser.execute_script("return document.getElementsByClassName('ngx-dropdown-button')[0]")
     country.click()
-    #country.send_keys("Israel")
-    #country.click()
-    #country_name = browser.find_element_by_xpath("/html/body/app-root/app-layout/app-registration-form/form/div/div[1]/div/div[7]/div[3]/ngx-select-dropdown/div/div/ul[1]/li")
-    #country_name.click()
-    privacy = browser.find_element_by_xpath("/html/body/app-root/app-layout/app-registration-form/form/div/div[2]/div/div[2]/label")
-    privacy.click()
-    cap = browser.find_element_by_xpath("/html/body/div[2]/div[3]/div[1]/div/div/span/div[1]")
-    cap.click()
+    country.send_keys(Keys.TAB)
+    for i in range(0, 107):
+        country.send_keys(Keys.DOWN)
+    sleepy.sleep(2)
+    country.send_keys(Keys.RETURN)
+
+    browser.execute_script("document.getElementsByClassName('custom-control custom-checkbox')[1].children[0].click()")
+
+    cap = browser.execute_script("return document.getElementsByClassName('custom-control custom-checkbox')[1].nextElementSibling.children[0]")
+    cap = cap.find_elements(By.TAG_NAME, 'iframe')[0]
+
+    browser.switch_to.frame(cap)
+
+    browser.execute_script("document.getElementsByClassName('recaptcha-checkbox-border')[0].click()")
+    sleepy.sleep(5)
+
+    browser.switch_to.default_content()
+    browser.switch_to.frame(browser.find_element(By.ID, 'calculatorFrame'))
+
+    sleepy.sleep(5)
     get_license = browser.find_element_by_xpath("/html/body/app-root/app-layout/app-registration-form/form/div/div[2]/div/button")
     get_license.click()
     sleepy.sleep(5)
@@ -103,7 +135,6 @@ while 1:
 
     print(time, ": Work.       ( work", bumps, ")")
     print()
-    #exit 0
     countdown = 86400
 
     for i in range(0, 8):
@@ -111,3 +142,4 @@ while 1:
         countdown = countdown - 900
         print('Next bump in: ', minutes, 'minutes.' )
         sleepy.sleep(900)
+       
